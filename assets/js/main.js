9ed584129c3114ae3f4d99f39a896f1d1c2ad4a0
Kartavya Jharwal <kjisgreatforever@gmail.com>
2023-10-21 23:52:22 +0530

    Test commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -256,3 +256,9 @@
 		}
 
 })(jQuery);
+
+
+/*  clock */
+const hours = document.querySelector('.hours');
+const minutes = document.querySelector('.minutes');
+const seconds = document.querySelector('.seconds');
